3d83106ed3b6d702c9bfcaf8ed0ecdef1e884096
Purvi-modi <purvimodi1997@gmail.com>
2021-01-27 12:38:43 +0530

    reverse iterator

--- iterator.py
+++
@@ -1,9 +0,0 @@
-# 10. Write an iterator class ReverseIter, that takes a list and iterates it from the reverse
-# Direction.
-
-
-my_list = [10,20,30,40,50,60,70]
-
-my_iter= iter(my_list)
-
-size = len(my_iter)
